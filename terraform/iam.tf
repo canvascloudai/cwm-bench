8df10ffee49d5b828ecccb39bfbb1c0d82c63beb
canvascloudai <kevin@canvascloud.ai>
2026-08-29 16:49:31 -0400

    Add EC2 IAM for SSM and DB password

---
+++ terraform/iam.tf
@@ -0,0 +1,50 @@
+data "aws_iam_policy_document" "ec2_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ec2" {
+  name               = "${local.name}-ec2-${local.id_slug}"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+data "aws_iam_policy_document" "app_ssm_db" {
+  statement {
+    sid     = "ReadDbPassword"
+    actions = ["ssm:GetParameter"]
+    resources = [
+      aws_ssm_parameter.db_password.arn,
+    ]
+  }
+  statement {
+    sid       = "DecryptDbPassword"
+    actions   = ["kms:Decrypt"]
+    resources = ["*"]
+    condition {
+      test     = "StringEquals"
+      variable = "kms:ViaService"
+      values   = ["ssm.${var.region}.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "app_ssm_db" {
+  name   = "read-db-password"
+  role   = aws_iam_role.ec2.id
+  policy = data.aws_iam_policy_document.app_ssm_db.json
+}
+
+resource "aws_iam_instance_profile" "ec2" {
+  name = "${local.name}-ec2-${local.id_slug}"
+  role = aws_iam_role.ec2.name
+}
